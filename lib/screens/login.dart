902e47012ca928d84a02bcd7fc666ef0b55eb260
siem <siemarell@gmail.com>
2020-01-22 17:33:02 +0300

    login

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -4,23 +4,39 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-//        appBar: AppBar(title: Text('foo')),
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                decoration: BoxDecoration(color: Colors.red),
-                child: Text('R'),
-              ),
-              Container(
-                decoration: BoxDecoration(color: Colors.green),
-                child: Text('G'),
-              ),
-              Container(
-                decoration: BoxDecoration(color: Colors.blue),
-                child: Text('B'),
-              )
-            ]));
+        appBar: AppBar(
+          title: Center(child: Text('Login')),
+        ),
+        body: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/blue_bg.jpeg"),
+                    fit: BoxFit.cover)),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  FlatButton(
+                    color: Colors.red,
+                    textColor: Colors.white,
+                    padding: EdgeInsets.all(8.0),
+                    child: Center(child: Text('GOOGLE')),
+                    onPressed: () => {},
+                  ),
+                  FlatButton(
+                    color: Colors.green,
+                    textColor: Colors.white,
+                    padding: EdgeInsets.all(8.0),
+                    child: Center(child: Text('FACEBOOK')),
+                    onPressed: () => {},
+                  ),
+                  FlatButton(
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    padding: EdgeInsets.all(8.0),
+                    child: Center(child: Text('TWITTER')),
+                    onPressed: () => {},
+                  ),
+                ])));
   }
 }
